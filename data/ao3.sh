761618dc35c4d578078624dc8acd9bd0357d44fd
Olaf Bernstein <camel-cdr@protonmail.com>
2022-02-13 22:59:19 +0100

    fixed again, this should work now

--- data/ao3.sh
+++ data/ao3.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#/bin/sh
 
 if [ $# -ne 1 ] ; then
 	echo "Usage: $0 ao3-tags-url"
@@ -9,20 +9,19 @@ fi
 curl "$1" |
 grep -A 2 ">Relationships:<" |
 grep '<li>.*<\/li>' |
-sed 's/\s*<li><a class="tag" href="\([^"]*\)">\([^<]*\)<\/a><\/li>/\2,https:\/\/www.archiveofourown.org\1\n/g' |
+sed 's/\s*<li><a class="tag" href="\([^"]*\)">\([^<]*\)<\/a><\/li>/\2,https:\/\/archiveofourown.org\1\n/g' |
 sed '/&amp;/d'  |
 sed '/^[^,]*\/[^,]*\//d' > shiplist.csv
 
-lines=$(cat shiplist.csv)
 IFS=$'\n'
-for s in $lines
+for s in `cat shiplist.csv`
 do
 	ship=`echo "$s" | cut -d\, -f1 | sed 's/\//,/g'`
 	url=`echo "$s" | cut -d\, -f2`
 	sleep 0.1
-	curl "$url" | grep 'has been made a synonym of' >/dev/null && continue
+	curl $url | grep 'has been made a synonym of' >/dev/null && continue
 	sleep 0.1
 	printf "%s,%s\n" `curl "$url/works" | grep "[0-9]* Works in" | sed 's/^.*\([0-9][0-9]*\) Works in.*$/\1/'` "$ship"
-done > counts.csv
+done | tee counts.csv
 
 sort -n -t ',' -k 3 counts.csv > sorted.csv
